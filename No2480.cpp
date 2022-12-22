469a4af4a99f7a3c1ca0ad0a565c8b88161bf2b7
leehj04 <leeeehj04@gmail.com>
2022-12-23 02:23:10 +0900

    22/12/22

---
+++ No2480.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,y,z;
+    cin >> x >> y >> z;
+    if((x==y) && (y==z)){
+        cout << 10000 + x*1000 << endl;
+    } else if(x==y){
+        cout << 1000 + x*100 << endl;
+    } else if(y==z){
+        cout << 1000 + y*100 << endl;
+    } else if(z==x){
+        cout << 1000 + z*100 << endl;
+    } else{
+        cout << max(max(x,y),z)*100 << endl;
+    }
+}
