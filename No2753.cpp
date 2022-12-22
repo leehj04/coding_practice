7da7a2a26c272f3efe4f6898c1e3fc8d0fe1ade3
leehj04 <leeeehj04@gmail.com>
2022-12-23 01:45:09 +0900

    22/12/22

---
+++ No2753.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    if (((x%4 == 0) && (x%100 != 0)) || ((x%4 == 0) && (x%400 == 0))){
+        cout << "1" << endl;
+    } else {
+        cout << "0" << endl;
+    }
+}
