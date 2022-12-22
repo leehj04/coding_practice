ec7b470750fc0cf49178cafc094a17097f963cb9
leehj04 <leeeehj04@gmail.com>
2022-12-23 01:06:48 +0900

    Create No2588.cpp

---
+++ No2588.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+
+    cin >> x >> y;
+    cout << x*(y%10) << endl;
+    cout << x*((y/10)%10) << endl;
+    cout << x*(y/100) << endl;
+
+    cout << (x*(y%10)) + (x*((y/10)%10))*10 + (x*(y/100))*100 << endl;
+}
