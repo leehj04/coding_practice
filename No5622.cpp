f7baa2569a0327d5194d92391928edeffa64be75
leehj04 <leeeehj04@gmail.com>
2023-01-08 02:12:38 +0900

    23/01/07

---
+++ No5622.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string call;
+    cin >> call;
+    int time = 0;
+    for (int i = 0; i < call.length(); i++) {
+        switch (call[i])
+        {
+            case 'A':
+            case 'B':
+            case 'C':
+                time += 3;
+                break;
+            case 'D':
+            case 'E':
+            case 'F':
+                time += 4;
+                break;
+            case 'G':
+            case 'H':
+            case 'I':
+                time += 5;
+                break;
+            case 'J':
+            case 'K':
+            case 'L':
+                time += 6;
+                break;
+            case 'M':
+            case 'N':
+            case 'O':
+                time += 7;
+                break;
+            case 'P':
+            case 'Q':
+            case 'R':
+            case 'S':
+                time += 8;
+                break;
+            case 'T':
+            case 'U':
+            case 'V':
+                time += 9;
+                break;
+            case 'W':
+            case 'X':
+            case 'Y':
+            case 'Z':
+                time += 10;
+                break;
+        }
+    }
+    cout << time;
+}
